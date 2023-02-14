845cc24ba946a4375d3004a2058762a61f0786b1
Imranahmed223 <upwork@elite-it.co.uk>
2023-02-15 02:07:12 +0500

    NAvbar

--- src/Components/NavBar/Navbar.jsx
+++ src/Components/NavBar/Navbar.jsx
@@ -1,13 +1,26 @@
 import React from 'react'
 import './Navbar.scss';
+import Logo from '../../assets/NavAssets/logo.png';
 const Navbar = () => {
   return (
     <>
-    <div className='navbar-container'>
-        <div className='navbar-container-content'></div>
-    </div>
+      <div className="navbar-container">
+        <div className="navbar-container-content">
+          <div className="navbar-container-content-left">
+            <img src={Logo} alt="Logo" />
+            <h1>Hoo</h1>
+            <span>Bank</span>
+          </div>
+          <div className="navbar-container-content-right">
+            <p>Home</p>
+            <p>About Us</p>
+            <p>Features</p>
+            <p>Solution</p>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
 export default Navbar
