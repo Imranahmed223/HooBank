735b572a2d5cb603aa73f8e8ea17d7d40937cb52
Imranahmed223 <upwork@elite-it.co.uk>
2023-02-15 04:20:04 +0500

    subheader

--- src/Components/modules/home/Home.jsx
+++ src/Components/modules/home/Home.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import "./Home.scss";
+import Hanheader from "../../../assets/Handheader.svg";
+import Persentage from '../../../assets/homeAssets/persntage.png';
 const Home = () => {
   return (
     <>
       <div className="subheader-container">
         <div className="subheader-container-content">
-          <div className="subheader-container-content-left"></div>
-          <div className="subheader-container-content-right"></div>
+          <div className="subheader-container-content-left">
+            <div className="subheader-container-content-left-discountbox">
+                <img src={Persentage} alt="persentage" />
+            </div>
+          </div>
+          <div className="subheader-container-content-right">
+            <img src={Hanheader} alt="subheader-Img" />
+          </div>
         </div>
       </div>
     </>
