d7c3abd2174d27a25f1380daf97f40a4fd742bc3
M-Zeeshan6886 <mzxeeshuu@gmail.com>
2023-02-15 00:36:24 +0500

    HooBank

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+// import reactLogo from './assets/react.svg'
+import './App.css'
+import Navbar from './Components/NavBar/Navbar'
+
+function App() {
+  
+  return (
+    <>
+    <Navbar />
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/NavBar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Navbar.scss';
+const Navbar = () => {
+  return (
+    <>
+    <div className='navbar-container'>
+        <div className='navbar-container-content'></div>
+    </div>
+    </>
+  )
+}
+
+export default Navbar
