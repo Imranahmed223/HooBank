00285dc59f9746af959440a32a5b248cb28394e5
Imranahmed223 <upwork@elite-it.co.uk>
2023-02-15 05:10:25 +0500

    Subheader s

--- src/Components/modules/home/Home.jsx
+++ src/Components/modules/home/Home.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import "./Home.scss";
 import Hanheader from "../../../assets/Handheader.svg";
-import Persentage from '../../../assets/homeAssets/persntage.png';
+import Persentage from "../../../assets/homeAssets/persntage.png";
+import GetStart from '../../../assets/homeAssets/getstrt.png';
 const Home = () => {
   return (
     <>
@@ -9,11 +10,22 @@ const Home = () => {
         <div className="subheader-container-content">
           <div className="subheader-container-content-left">
             <div className="subheader-container-content-left-discountbox">
-                <img src={Persentage} alt="persentage" />
-                <p>
-                    <span>20%</span> DISCOUNT FOR <span>1 MONTH</span> ACCOUNT
-                </p>
+              <img src={Persentage} alt="persentage" />
+              <p>
+                <span>20%</span> DISCOUNT FOR <span>1 MONTH</span> ACCOUNT
+              </p>
             </div>
+            <div className="subheader-container-content-left-headtext">
+              <p>The Next</p>
+              <span>Generation</span>
+              <p>Payment Method.</p>
+            </div>
+            <img src={GetStart} alt="GetStart" />
+            <p>
+              Our team of experts uses a methodology to identify the credit
+              cards most likely to fit your needs. We examine annual percentage
+              rates, annual fees.
+            </p>
           </div>
           <div className="subheader-container-content-right">
             <img src={Hanheader} alt="subheader-Img" />
